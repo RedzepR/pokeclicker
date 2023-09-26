8f4f086e9321fd98be0163db2af33d13e0a812a2
RedzepR <143606926+RedzepR@users.noreply.github.com>
2023-09-26 17:34:03 +0200

    Bugfix breeding debuff sort

--- src/scripts/breeding/BreedingController.ts
+++ src/scripts/breeding/BreedingController.ts
@@ -177,7 +177,7 @@ class BreedingController {
         if (App.game.challenges.list.regionalAttackDebuff.active()) {
             // Check if regional debuff being applied for sorting
             if (BreedingController.regionalAttackDebuff() > -1 && PokemonHelper.calcNativeRegion(pokemon.name) !== BreedingController.regionalAttackDebuff()) {
-                return App.game.party.getRegionAttackMultiplier(player.region, pokemon.id);
+                return App.game.party.getRegionAttackMultiplier(BreedingController.regionalAttackDebuff(), pokemon.id);
             }
         }
         return 1.0;
