41486e2c9c3bd84d6ea2390563f5bd39d73c47c3
RedzepR <143606926+RedzepR@users.noreply.github.com>
2025-08-19 06:37:18 +0200

    guzzlord more beastball

--- src/scripts/dungeons/Dungeon.ts
+++ src/scripts/dungeons/Dungeon.ts
@@ -12621,9 +12621,12 @@ dungeonList['Resolution Cave'] = new Dungeon('Resolution Cave',
         epic: [
             {loot: 'Dread Plate'},
             {loot: 'Draco Plate'},
-            {loot: 'Beastball', requirement: new QuestLineStepCompletedRequirement('Ultra Beast Hunt', 1) },
+            
         ],
-        legendary: [{loot: 'Dragon_Fang'}],
+        legendary: [
+            { loot: 'Dragon_Fang' },
+            {loot: 'Beastball', requirement: new QuestLineStepCompletedRequirement('Ultra Beast Hunt', 1), amount: 3 },
+        ],
     },
     17114462,
     [
