c32908368c3e28f39d7508f744efd90f53c2e4f2
RedzepR <143606926+RedzepR@users.noreply.github.com>
2026-01-15 15:58:13 +0100

    Added weather to all calcs, defeat dungeon quest remove click attack consideration, removes 4hit limit in reward calculation

--- src/scripts/quests/questTypes/DefeatGymQuest.ts
+++ src/scripts/quests/questTypes/DefeatGymQuest.ts
@@ -39,7 +39,7 @@ class DefeatGymQuest extends Quest implements QuestInterface {
     private static calcReward(amount: number, gymTown: string): number {
         const gym = GymList[gymTown];
         const town = gym.parent;
-        const playerDamage = App.game.party.calculatePokemonAttack(undefined, undefined, undefined, town.region, true, undefined, undefined, true, false, town.subRegion);
+        const playerDamage = App.game.party.calculatePokemonAttack(undefined, undefined, undefined, town.region, true, undefined, Weather.regionalWeather[town.region](), true, false, town.subRegion);
         let attacksToWin = 0;
         for (const pokemon of gym.getPokemonList()) {
             attacksToWin += Math.ceil( Math.min( 4, pokemon.maxHealth / Math.max(1, playerDamage) ) );
